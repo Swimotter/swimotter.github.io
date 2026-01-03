c2dabfa56142cece28c41043ba4c26866f799ef6
Jackson (Swimotter) Rubiano <mr.jacksonrubiano@gmail.com>
2026-01-03 08:12:19 +0000

    feat: Add page footer

--- personal-site/app/layout.tsx
+++ personal-site/app/layout.tsx
@@ -3,6 +3,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import Image from "next/image";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -21,6 +22,24 @@ const navItems = [
   { name: "Resume", href: "/resume" },
 ];
 
+const extLinks = [
+  {
+    name: "Mail",
+    href: "mailto:mr.jacksonrubiano@gmail.com",
+    alt: "Send me an email!",
+  },
+  {
+    name: "Github",
+    href: "https://github.com/swimotter",
+    alt: "Check out my GitHub!",
+  },
+  {
+    name: "LinkedIn",
+    href: "https://linkedin.com/in/jackson-rubiano",
+    alt: "Connect with me on LinkedIn!",
+  },
+];
+
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -33,35 +52,64 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <header className="sticky top-0 z-50 bg-gray-900">
-          <div className="flex items-center w-full mx-auto px-4 py-3 sm:px-6">
-            <Link href="/" className="font-extrabold text-white">
-              Jackson (Swimotter) Rubiano
-            </Link>
-            <nav className="flex items-center gap-4 ml-auto">
-              {navItems.map((item) => {
-                const isActive = pathname === item.href;
+        <div className="min-h-screen flex flex-col w-full">
+          <header className="sticky top-0 z-50 bg-gray-900">
+            <div className="flex items-center w-full mx-auto px-4 py-3 sm:px-6">
+              <Link href="/" className="font-extrabold text-white">
+                Jackson (Swimotter) Rubiano
+              </Link>
+              <nav className="flex items-center gap-4 ml-auto">
+                {navItems.map((item) => {
+                  const isActive = pathname === item.href;
 
-                return (
-                  <Link
-                    key={item.name}
-                    href={item.href}
-                    className={`flex items-center transition-colors
-                      ${
-                        isActive
-                          ? "text-cyan-600 font-medium border-blue-600"
-                          : "text-gray-200 hover:text-cyan-800 border-transparent"
-                      }
-                    `}
-                  >
-                    {item.name}
-                  </Link>
-                );
-              })}
-            </nav>
-          </div>
-        </header>
-        <main>{children}</main>
+                  return (
+                    <Link
+                      key={item.name}
+                      href={item.href}
+                      className={`flex items-center transition-colors
+                        ${
+                          isActive
+                            ? "text-cyan-600 font-medium border-blue-600"
+                            : "text-gray-200 hover:text-cyan-800 border-transparent"
+                        }
+                      `}
+                    >
+                      {item.name}
+                    </Link>
+                  );
+                })}
+              </nav>
+            </div>
+          </header>
+
+          <main className="flex flex-1">{children}</main>
+
+          <footer className="bottom-0 z-50 bg-gray-950 max-h-17.5 overflow-hidden">
+            <div className="flex items-center w-full mx-auto px-4 py-3 sm:px-6">
+              <nav className="w-full flex items-center justify-center gap-4">
+                {extLinks.map((item) => {
+                  return (
+                    <a
+                      key={item.name}
+                      href={item.href}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      <Image
+                        className={`${true ? "invert" : ""}`}
+                        src={`/${item.name}.svg`}
+                        alt={`${item.alt}`}
+                        width={50}
+                        height={50}
+                        priority
+                      />
+                    </a>
+                  );
+                })}
+              </nav>
+            </div>
+          </footer>
+        </div>
       </body>
     </html>
   );
--- personal-site/app/page.tsx
+++ personal-site/app/page.tsx
@@ -8,10 +8,10 @@ export const metadata: Metadata = {
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen w-full flex-col items-center justify-between py-32 px-16 bg-white dark:bg-gray-800 sm:items-start">
+    <main className="flex w-full flex-col items-center justify-between py-8 px-8 bg-white dark:bg-gray-800">
       <section className="mx-auto px-4 py-12 sm:px-6 flex flex-col md:flex-row items-start gap-8">
         <div className="flex-1 text-left">
-          <p className="text-4xl mb-4 text-black dark:text-zinc-50">
+          <p className="text-2xl mb-4 text-black dark:text-zinc-50">
             Hello everyone! You will find some of the cool things that I’ve
             worked on and been a part of on this site. Many of these are the
             result of my schooling at Bucknell University, with some being
@@ -27,7 +27,7 @@ export default function Home() {
             height={250}
             priority
           />
-          <p className="w-full text-black dark:text-zinc-50 text-2x1 text-left md:text-left max-w-sm">
+          <p className="w-full text-black dark:text-zinc-50 text-left md:text-left max-w-sm">
             Jackson (Swimotter) Rubiano<br></br>
             Bucknell University
           </p>
