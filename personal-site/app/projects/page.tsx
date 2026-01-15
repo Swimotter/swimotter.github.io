bf13a476b0711fee35aa9c958522bcf2c37cedd2
Jackson (Swimotter) Rubiano <mr.jacksonrubiano@gmail.com>
2026-01-15 10:06:37 +0000

    feat: Create projects page

---
+++ personal-site/app/projects/page.tsx
@@ -0,0 +1,36 @@
+import type { Metadata } from "next";
+
+import Project from "@/components/project";
+import FilmRoll from "@/public/film-roll.jpg";
+
+export const metadata: Metadata = {
+  title: "Projects | Jackson (Swimotter) Rubiano",
+  description: "Projects worked on by Jackson (Swimotter) Rubiano",
+};
+
+const projects = [
+  {
+    title: "The Japanese Paper Film Project",
+    description: "Some photos and highlights from my time on the project",
+    img: FilmRoll,
+    page: "paper-films",
+  },
+];
+
+export default function Home() {
+  return (
+    <section className="w-full grid grid-cols-[repeat(auto-fit,minmax(0,240))] gap-4 justify-center items-start">
+      {projects.map((item) => {
+        return (
+          <Project
+            key={item.page}
+            title={item.title}
+            description={item.description}
+            img={item.img}
+            page={`projects/${item.page}`}
+          />
+        );
+      })}
+    </section>
+  );
+}
