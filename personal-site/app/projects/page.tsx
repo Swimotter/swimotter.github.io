afea456ea551b1ee647a10cdbae2db2f71990213
Jackson (Swimotter) Rubiano <mr.jacksonrubiano@gmail.com>
2026-01-20 04:55:38 +0000

    feat: Sort projects by priority

--- personal-site/app/projects/page.tsx
+++ personal-site/app/projects/page.tsx
@@ -8,29 +8,35 @@ export const metadata: Metadata = {
   description: "Projects worked on by Jackson (Swimotter) Rubiano",
 };
 
+// Priority on a scale of 1-5
+// 1: Highest priority
+// 5: Lowest priority
 const projects = [
   {
     title: "The Japanese Paper Film Project",
     description: "Some photos and highlights from my time on the project",
     img: FilmRoll,
     page: "paper-films",
+    priority: 1,
   },
 ];
 
 export default function Home() {
   return (
     <section className="w-4/5 grid grid-cols-[repeat(auto-fit,minmax(0,240))] gap-4 justify-center items-start">
-      {projects.map((item) => {
-        return (
-          <Project
-            key={item.page}
-            title={item.title}
-            description={item.description}
-            img={item.img}
-            page={`projects/${item.page}`}
-          />
-        );
-      })}
+      {projects
+        .sort((a, b) => a.priority - b.priority)
+        .map((item) => {
+          return (
+            <Project
+              key={item.page}
+              title={item.title}
+              description={item.description}
+              img={item.img}
+              page={`projects/${item.page}`}
+            />
+          );
+        })}
     </section>
   );
 }
