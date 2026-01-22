b1cc9f4e999030d26e9e0c494a9b471119b66515
Jackson (Swimotter) Rubiano <mr.jacksonrubiano@gmail.com>
2026-01-22 03:33:15 +0000

    style: Update link color and underlining

--- personal-site/app/projects/paper-films/page.tsx
+++ personal-site/app/projects/paper-films/page.tsx
@@ -26,7 +26,7 @@ export default function Home() {
               target="_blank"
               rel="noopener noreferrer"
             >
-              <span className="text-blue-400 underline">
+              <span className="text-blue-500 hover:underline">
                 The Japanese Paper Film Project
               </span>
             </a>{" "}
