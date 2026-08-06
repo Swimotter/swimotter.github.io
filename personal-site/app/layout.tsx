b3bd2dcd45acf55c7818ba4bcdcdd45dcb74391f
Jackson (Swimotter) Rubiano <mr.jacksonrubiano@gmail.com>
2026-08-06 23:29:23 +0000

    fix: switch to using nextjs GoogleTagManager

--- personal-site/app/layout.tsx
+++ personal-site/app/layout.tsx
@@ -5,10 +5,10 @@ import "@/app/globals.css";
 import localFont from "next/font/local";
 import { Inter } from "next/font/google";
 
+import { GoogleTagManager } from "@next/third-parties/google";
+
 import { Header } from "@/components/header";
 import { Footer } from "@/components/footer";
-
-import Script from "next/script";
 
 const cabinetGrotesk = localFont({
   src: "../fonts/CabinetGrotesk-Variable.woff2",
@@ -30,28 +30,8 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className={`${cabinetGrotesk.variable} ${inter.variable}`}>
-      <head>
-        <Script defer id="gtm-script" strategy="afterInteractive">
-          {`
-(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','${process.env.NEXT_PUBLIC_GTM_ID}');
-`}
-        </Script>
-      </head>
-
+      <GoogleTagManager gtmId={`${process.env.NEXT_PUBLIC_GTM_ID}`} />
       <body>
-        <noscript>
-          <iframe
-            src={`https://www.googletagmanager.com/ns.html?id=${process.env.NEXT_PUBLIC_GTM_ID}`}
-            height="0"
-            width="0"
-            style={{ display: "none", visibility: "hidden" }}
-          />
-        </noscript>
-
         <div className="flex min-h-screen w-full flex-col">
           <Header />
           <div className="mx-auto flex w-full flex-1 justify-center p-4 md:w-4/5 xl:w-2/3">
