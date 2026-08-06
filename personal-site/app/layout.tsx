ebf77f186efa84dddf703769381c9562bb1b06f2
Jackson (Swimotter) Rubiano <mr.jacksonrubiano@gmail.com>
2026-08-06 22:41:39 +0000

    feat: add google analytics

--- personal-site/app/layout.tsx
+++ personal-site/app/layout.tsx
@@ -8,6 +8,8 @@ import { Inter } from "next/font/google";
 import { Header } from "@/components/header";
 import { Footer } from "@/components/footer";
 
+import Script from "next/script";
+
 const cabinetGrotesk = localFont({
   src: "../fonts/CabinetGrotesk-Variable.woff2",
   variable: "--font-cabinet",
@@ -28,7 +30,28 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className={`${cabinetGrotesk.variable} ${inter.variable}`}>
+      <head>
+        <Script defer id="gtm-script" strategy="afterInteractive">
+          {`
+(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','${process.env.GTM_CONTAINER_ID}');
+`}
+        </Script>
+      </head>
+
       <body>
+        <noscript>
+          <iframe
+            src={`https://www.googletagmanager.com/ns.html?id=${process.env.GTM_CONTAINER_ID}`}
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
+
         <div className="flex min-h-screen w-full flex-col">
           <Header />
           <div className="mx-auto flex w-full flex-1 justify-center p-4 md:w-4/5 xl:w-2/3">
